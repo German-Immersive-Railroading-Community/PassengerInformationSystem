618f8d7ee2b06b5504e19611a912781d53468c72
Der-Zauberer <and.som282@gmail.com>
2022-05-20 09:26:13 +0200

    Reorganized request and html classes

--- src/eu/girc/pis/requests/SeachRequest.java
+++ src/eu/girc/pis/requests/SeachRequest.java
@@ -1,22 +1,55 @@
 package eu.girc.pis.requests;
 
-import eu.girc.pis.html.SearchHtml;
+import eu.girc.pis.components.Line;
+import eu.girc.pis.components.Station;
+import eu.girc.pis.html.Html;
+import eu.girc.pis.html.HtmlTag;
+import eu.girc.pis.main.Main;
 import eu.girc.pis.main.RequestHandler;
+import eu.girc.pis.resources.Resource;
+import eu.girc.pis.util.Entity;
+import eu.girc.pis.util.EntityList;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 
 public class SeachRequest implements HttpHandler {
+	
+	private static String searchBar = Resource.getTextFile("search.html");
 
 	@Override
 	public void handleRequest(HttpServerExchange exchange) throws Exception {
 		String args[] = RequestHandler.getArgs(exchange.getRequestPath());
 		if (args.length == 1) {
-			RequestHandler.sendHtml(exchange, SearchHtml.buildSearch());
+			RequestHandler.sendHtml(exchange, buildSearch());
 		} else {
 			String search = args[1].replace('&', ' ');
-			RequestHandler.sendHtml(exchange, SearchHtml.buildSeachResults(search));
+			RequestHandler.sendHtml(exchange, buildSeachResults(search));
 		}
-		
+	}
+	
+	private static String buildSearch() {
+		return Html.buildHtml(searchBar, false);
+	}
+	
+	private static String buildSeachResults(String search) {
+		String string = searchBar;
+		string += new HtmlTag("h2", "Search results for \"" + search + "\"");
+		EntityList<Entity> entities = new EntityList<>();
+		Main.getStations().forEach(station -> entities.add(station));
+		Main.getLines().forEach(line -> entities.add(line));
+		EntityList<Entity> results = entities.sort().searchForDisplayName(search);
+		if (results.size() > 0) {
+			for (Entity entity : results) {
+				if (entity instanceof Station) {
+					string += StationRequest.buildStationPreview((Station) entity);
+				} else if (entity instanceof Line) {
+					string += LineRequest.buildLinePreview((Line) entity);
+				}
+			}
+		} else {
+			string += new HtmlTag("div", "No search results found!", "box").toString();
+		}
+		return Html.buildHtml(string, false);
 	}
 
 }
